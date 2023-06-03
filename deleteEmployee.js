5b7792e3dd3f16e0170c0e6b60b9360cde2d0025
Ovadya33 <timurgts2710@gmail.com>
2023-06-03 15:15:18 +0300

    first commit

---
+++ deleteEmployee.js
@@ -0,0 +1,8 @@
+const URL = 'http://localhost:3500/employees/126';
+
+fetch(URL, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },   
+})
+    .then((response) => response.json())
+    .then((data) => console.log(data));
