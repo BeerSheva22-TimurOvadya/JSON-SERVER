a587b83abc06711fac7081f1e22faabb040a3b39
Ovadya33 <timurgts2710@gmail.com>
2023-06-28 16:34:49 +0300

    classWork

--- addEmployee.js
+++ addEmployee.js
@@ -5,11 +5,17 @@ const empl = {
     salary: 10000,
     department: 'Development',
     birhYear: '2000',
+    userId: 'admin'
 };
+
 fetch(URL, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(empl)
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlbHJhbi1hZG1pbkBjby5pbCIsImlhdCI6MTY4Nzk1Njc4NiwiZXhwIjoxNjg3OTYwMzg2LCJzdWIiOiJhZG1pbiJ9.KpxEvOIGk2L4CUqE69CVMLwYmWraizhlxoC34L5sh-I',
+    },
+    body: JSON.stringify(empl),
 })
     .then((response) => response.json())
     .then((data) => console.log(data));
